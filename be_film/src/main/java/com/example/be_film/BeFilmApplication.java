43933faff9686409aa3bcd27e2608c963b241ce0
Him <vuqtpro1592002@gmail.com>
2024-11-25 17:53:05 +0700

    landau

---
+++ be_film/src/main/java/com/example/be_film/BeFilmApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeFilmApplication.class, args);
+	}
+
+}
